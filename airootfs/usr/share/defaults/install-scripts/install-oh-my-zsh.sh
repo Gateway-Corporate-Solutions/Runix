6a0e199149b6f2587a04fc32593a4c2044414349
Samuel Roux <samroux808@gmail.com>
2025-11-27 13:29:53 -0600

    Auto commit - Building...

--- airootfs/usr/share/defaults/install-scripts/install-oh-my-zsh.sh
+++ airootfs/usr/share/defaults/install-scripts/install-oh-my-zsh.sh
@@ -3,5 +3,6 @@
 mv $HOME/.zshrc $HOME/.zshrc.old
 curl -O https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 nohup sh install.sh &
+sleep 5
 cat $HOME/.zshrc.old >> $HOME/.zshrc
 rm install.sh
