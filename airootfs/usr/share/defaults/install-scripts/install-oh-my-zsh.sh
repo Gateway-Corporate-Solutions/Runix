bf3ada760e272ec409c92fc63fa208f12b2971e4
Samuel Roux <samroux808@gmail.com>
2025-11-27 11:17:07 -0600

    Runix KDE Tweaks install now sets theme to breezedark by default

--- airootfs/usr/share/defaults/install-scripts/install-oh-my-zsh.sh
+++ airootfs/usr/share/defaults/install-scripts/install-oh-my-zsh.sh
@@ -1,3 +1,4 @@
 #!/bin/zsh
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cat ~/.zshrc-pre-oh-my-zsh >> ~/.zshrc
+cat ~/.zshrc.pre-oh-my-zsh >> ~/.zshrc
--- airootfs/usr/share/defaults/install-scripts/install-runix-kde-tweaks.sh
+++ airootfs/usr/share/defaults/install-scripts/install-runix-kde-tweaks.sh
@@ -2,3 +2,5 @@
 
 echo "[Containments][28][Applets][78][Configuration][General]" >> $HOME/.config/plasma-org.kde.plasma.desktop-appletsrc
 echo "icon=$HOME/.config/icons/gem.svg" >> $HOME/.config/plasma-org.kde.plasma.desktop-appletsrc
+echo "[KDE]" >> $HOME/.config/kdeglobals
+echo "LookAndFeelPackage=org.kde.breezedark.desktop" >> $HOME/.config/kdeglobals
