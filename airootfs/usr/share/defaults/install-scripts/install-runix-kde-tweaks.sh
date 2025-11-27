0f4e6dea53dbbb6a0806bf8919aaca0b0ee326d6
Samuel Roux <samroux808@gmail.com>
2025-11-27 10:51:16 -0600

    Auto commit - Building...

---
+++ airootfs/usr/share/defaults/install-scripts/install-runix-kde-tweaks.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+echo "[Containments][28][Applets][78][Configuration][General]" >> $HOME/.config/plasma-org.kde.plasma.desktop-appletsrc
+echo "icon=$HOME/.config/icons/gem.svg" >> $HOME/.config/plasma-org.kde.plasma.desktop-appletsrc
