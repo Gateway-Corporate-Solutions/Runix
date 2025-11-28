78303c0261a9212829d16cca22d29a6ed9116abc
Samuel Roux <samroux808@gmail.com>
2025-11-28 10:47:15 -0600

    Auto commit - Building...

--- airootfs/usr/share/defaults/install-scripts/install-runix-cinnamon-tweaks.sh
+++ airootfs/usr/share/defaults/install-scripts/install-runix-cinnamon-tweaks.sh
@@ -4,3 +4,4 @@ cp $HOME/.local/share/config/cinnamon-menu.json $HOME/.config/cinnamon/spices/me
 gsettings set org.cinnamon.desktop.background picture-uri "file://$HOME/.local/share/backgrounds/Runix.png"
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+gsettings set org.cinnamon.desktop.interface gtk-theme 'Adwaita-dark'
