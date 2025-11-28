174631efef2d9bd7cc8b3c6d99a867dab47dcea3
Samuel Roux <samroux808@gmail.com>
2025-11-28 13:15:18 -0600

    Auto commit - Building...

--- airootfs/usr/share/defaults/install-scripts/install-runix-cinnamon-tweaks.sh
+++ airootfs/usr/share/defaults/install-scripts/install-runix-cinnamon-tweaks.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 cp $HOME/.local/share/config/cinnamon-menu.json $HOME/.config/cinnamon/spices/menu@cinnamon.org/0.json
-gsettings set org.cinnamon.desktop.background picture-uri "file://$HOME/.local/share/backgrounds/Runix.png"
+gsettings set org.cinnamon.desktop.background picture-uri "file://$HOME/.local/share/backgrounds/Gemscape.png"
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 gsettings set org.cinnamon.desktop.interface gtk-theme 'Adwaita-dark'
--- airootfs/usr/share/defaults/install-scripts/install-runix-kde-tweaks.sh
+++ airootfs/usr/share/defaults/install-scripts/install-runix-kde-tweaks.sh
@@ -3,6 +3,7 @@
 kquitapp6 plasmashell
 kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 2 --group Applets --group 3 --group Configuration --group General --key icon "$HOME/.local/share/icons/gem.svg"
 echo "\n[KDE]\nLookAndFeelPackage=org.kde.breezedark.desktop" >> $HOME/.config/kdeglobals
+plasma-apply-wallpaperimage "$HOME/.local/share/backgrounds/Gemscape.png"
 kstart5 plasmashell
 
 echo -e "\e[34m# KDE tweaks applied.\e[0m"
