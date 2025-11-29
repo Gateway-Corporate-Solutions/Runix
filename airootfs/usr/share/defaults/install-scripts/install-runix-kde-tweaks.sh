4541a91a3061a43a97588bcc7f72f46cf73f146d
Samuel Roux <samroux808@gmail.com>
2025-11-29 14:09:26 -0600

    Auto commit - Building...

--- airootfs/usr/share/defaults/install-scripts/install-runix-kde-tweaks.sh
+++ airootfs/usr/share/defaults/install-scripts/install-runix-kde-tweaks.sh
@@ -3,8 +3,9 @@
 kquitapp6 plasmashell
 kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 2 --group Applets --group 3 --group Configuration --group General --key icon "$HOME/.local/share/icons/gem.svg"
 echo "\n[KDE]\nLookAndFeelPackage=org.kde.breezedark.desktop" >> $HOME/.config/kdeglobals
-plasma-apply-wallpaperimage "$HOME/.local/share/backgrounds/Gemscape.png"
 kstart5 plasmashell
+sleep 5
+plasma-apply-wallpaperimage "$HOME/.local/share/backgrounds/Gemscape.png"
 
 echo -e "\e[34m# KDE tweaks applied.\e[0m"
 echo -e "\e[34m# Please reboot for all changes to take effect.\e[0m"
